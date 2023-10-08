ac73e4070dad75ed4dc6d8fc7975edcd7caef400
913165 <tinumistry@gmail.com>
2023-10-08 19:15:09 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import fastapi
+from student_data import students
+
+app = fastapi.FastAPI()
+
+# method to get all the student data
+@app.get("/students")
+async def get_students():
+    return students
+
+# method to get student data by student id
+@app.get("/students/{student_id}")
+async def get_student(student_id: int):
+    for student in students:
+        if student["id"] == student_id:
+            return student
+    raise fastapi.HTTPException(status_code=404, detail="student not found")
+
+# uvicorn command to run the app on port 8000
+# uvicorn main:app --reload --port 8000
+# command to create requirements.txt file
+# pip freeze > requirements.txt
+
+
+
+
+
